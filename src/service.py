2531c8874853c1fac0c5244d6d39edced3e84e81
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-05-24 23:55:43 +0600

    Create service.py

---
+++ src/service.py
@@ -0,0 +1,46 @@
+import amino
+from tabulate import tabulate
+from src import configs
+from src.utils import Login
+from src.utils import Communities
+from src.utils import Chats
+from src.scripts.raid_box import RaidBox
+from src.scripts.activity_box import ActivityBox
+from src.scripts.profile_box import ProfileBox
+from src.scripts.chat_box import ChatBox
+from src.scripts.other_box import OtherBox
+from src.scripts.account_box import AccountBox
+
+
+class MainApp:
+	def start(self):
+		self.client = amino.Client()
+		Login().login(self.client)
+		self.sub_client = amino.SubClient(
+			comId=Communities().communities(
+			self.client), profile=self.client.profile)
+		while True:
+			try:
+				print(
+					tabulate(
+						configs.MAIN_MENU,
+						headers=[
+							"",
+							configs.CATEGORIES[0],
+							"Authors"],
+						tablefmt="fancy_grid"))
+				select = int(input("[Select]::: "))
+				if select == 1:
+					RaidBox(self.client, self.sub_client).start()
+				elif select == 2:
+					ActivityBox(self.sub_client).start()
+				elif select == 3:
+					ProfileBox(self.client, self.sub_client).start()
+				elif select == 4:
+					ChatBox(self.client, self.sub_client).start()
+				elif select == 5:
+					OtherBox(self.client, self.sub_client).start()
+				elif select == 6:
+					AccountBox(self.client).start()
+			except Exception as e:
+				print(e)
